7d1a88432bbdabc0501ebdde5ff205a3a1580492
sanuranja <sanuranjan@netskope.com>
2023-03-30 21:09:05 +0530

    changes done

---
+++ build/app/generated/source/buildConfig/release/com/example/sudoku/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.sudoku;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.sudoku";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
